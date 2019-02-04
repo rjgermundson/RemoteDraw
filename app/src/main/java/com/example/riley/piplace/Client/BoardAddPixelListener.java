fd98f51ba29a95a23c1f6d2cc0036cf02a3b97e9
riley <rjgermundson@gmail.com>
2019-02-03 20:51:06 -0800

    Added complete line drawing to client

--- app/src/main/java/com/example/riley/piplace/Client/BoardAddPixelListener.java
+++ app/src/main/java/com/example/riley/piplace/Client/BoardAddPixelListener.java
@@ -10,7 +10,10 @@ import android.view.MotionEvent;
 import android.view.View;
 
 import com.example.riley.piplace.BoardActivity.BoardActivity;
+import com.example.riley.piplace.BoardActivity.ServerBoardActivity;
 import com.example.riley.piplace.Messages.Lines.Line;
+import com.example.riley.piplace.Server.CommunicateTask.ServerListenThread;
+import com.example.riley.piplace.Server.LockedBitmap;
 
 import java.util.Locale;
 import java.util.concurrent.BlockingQueue;
@@ -21,6 +24,8 @@ import java.util.concurrent.BlockingQueue;
 public class BoardAddPixelListener implements View.OnTouchListener {
     private BlockingQueue<Line> messageQueue;  // Pipeline to server
     private Bitmap pixelBoard;
+    private int prevX;
+    private int prevY;
 
     public BoardAddPixelListener(Bitmap pixelBoard,
                                  BlockingQueue<Line> messageQueue) {
@@ -39,21 +44,22 @@ public class BoardAddPixelListener implements View.OnTouchListener {
      */
     @Override
     public boolean onTouch(View board, MotionEvent event) {
+        // Round the height and weight to be exactly divisible by pixel dimensions
+        int width = (board.getWidth() / BoardActivity.BOARD_PIXEL_WIDTH) * BoardActivity.BOARD_PIXEL_WIDTH;
+        int height = (board.getHeight() / BoardActivity.BOARD_PIXEL_HEIGHT) * BoardActivity.BOARD_PIXEL_HEIGHT;
+        int widthStretch = width / BoardActivity.BOARD_PIXEL_WIDTH;
+        int heightStretch = height / BoardActivity.BOARD_PIXEL_HEIGHT;
+
+        int x = BoardActivity.BOARD_PIXEL_WIDTH * Math.round(event.getX()) / width;
+        int y = BoardActivity.BOARD_PIXEL_HEIGHT * Math.round(event.getY()) / height;
+        int color = BoardActivity.getColor();
+
         if (event.getAction() == MotionEvent.ACTION_DOWN) {
-            System.out.println("DOWN");
-            // Round the height and weight to be exactly divisible by pixel dimensions
-            int width = (board.getWidth() / BoardActivity.BOARD_PIXEL_WIDTH) * BoardActivity.BOARD_PIXEL_WIDTH;
-            int height = (board.getHeight() / BoardActivity.BOARD_PIXEL_HEIGHT) * BoardActivity.BOARD_PIXEL_HEIGHT;
-
-            int widthStretch = width / BoardActivity.BOARD_PIXEL_WIDTH;
-            int heightStretch = height / BoardActivity.BOARD_PIXEL_HEIGHT;
-
-            int x = BoardActivity.BOARD_PIXEL_WIDTH * Math.round(event.getX()) / width;
-            int y = BoardActivity.BOARD_PIXEL_HEIGHT * Math.round(event.getY()) / height;
+            prevX = x;
+            prevY = y;
 
             // Draw the change on the local board
             Canvas canvas = new Canvas(pixelBoard);
-            int color = BoardActivity.getColor();
             Paint paint = new Paint();
             paint.setColor(color);
             canvas.drawRect(x * widthStretch,
@@ -71,8 +77,35 @@ public class BoardAddPixelListener implements View.OnTouchListener {
             Line line = new Line(color, 0);
             line.addPixel(new Pair<>(x, y));
             messageQueue.add(line);
-        } else if (event.getActionMasked() == MotionEvent.ACTION_MOVE) {
-            System.out.println("MOVE");
+        } else if (event.getAction() == MotionEvent.ACTION_MOVE && event.getHistorySize() > 0) {
+            Canvas canvas = new Canvas(pixelBoard);
+            Paint paint = new Paint();
+            paint.setColor(color);
+            Line line = new Line(color, -1);
+            line.addPixel(new Pair<>(prevX, prevY));
+            while (prevX != x || prevY != y) {
+                if (prevX < x) {
+                    prevX++;
+                } else if (prevX > x) {
+                    prevX--;
+                }
+                if (prevY < y) {
+                    prevY++;
+                } else if (prevY > y) {
+                    prevY--;
+                }
+                canvas.drawRect(prevX * widthStretch,
+                        prevY * heightStretch,
+                        prevX * widthStretch + widthStretch,
+                        prevY * heightStretch + heightStretch,
+                        paint);
+                line.addPixel(new Pair<>(prevX, prevY));
+            }
+            messageQueue.add(line);
+
+            Message message = new Message();
+            message.what = BoardActivity.MESSAGE_REFRESH_BOARD;
+            ServerBoardActivity.updateHandler.sendMessage(message);
         } else {
             board.performClick();
         }
